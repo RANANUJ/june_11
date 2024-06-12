2a75185aae0559227cc1b926017979435cef6a43
RANANUJ <rana1452005@gmail.com>
2024-06-12 19:27:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,46 +20,44 @@ class MainApp extends StatelessWidget {
             child: Column(
 //                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Container(
-                          width: 100,
-                          height: 100,
-                          color: Colors.amber,
-                          margin: EdgeInsets.all(10),
-                          padding: EdgeInsets.all(10),
-                          alignment: Alignment.center,
-                          child: const Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [Icon(Icons.home), Text("Home")],
-                          ),
-                        ),
-                        Container(
-                          width: 100,
-                          height: 100,
-                          color: Colors.grey,
-                          margin: EdgeInsets.all(10),
-                          padding: EdgeInsets.all(10),
-                          alignment: Alignment.center,
-                          child: const Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [Icon(Icons.email), Text("Email")],
-                          ),
-                        ),
-                        Container(
-                          width: 100,
-                          height: 100,
-                          color: Colors.orange[300],
-                          margin: EdgeInsets.all(10),
-                          padding: EdgeInsets.all(10),
-                          alignment: Alignment.center,
-                          child: const Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [Icon(Icons.alarm), Text("Alarm")],
-                          ),
-                        ),
-                      ]),
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.amber,
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(10),
+                      alignment: Alignment.center,
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Icon(Icons.home), Text("Home")],
+                      ),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.grey,
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(10),
+                      alignment: Alignment.center,
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Icon(Icons.email), Text("Email")],
+                      ),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.orange[300],
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(10),
+                      alignment: Alignment.center,
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Icon(Icons.alarm), Text("Alarm")],
+                      ),
+                    ),
+                  ]),
                   Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                     Container(
                       width: 100,
@@ -98,43 +96,81 @@ class MainApp extends StatelessWidget {
                       ),
                     ),
                   ]),
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Color.fromARGB(255, 198, 78, 234),
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(10),
+                      alignment: Alignment.center,
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Icon(Icons.book), Text("Book")],
+                      ),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Color.fromARGB(255, 145, 83, 13),
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(10),
+                      alignment: Alignment.center,
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Icon(Icons.camera), Text("Camera")],
+                      ),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Color.fromARGB(255, 134, 183, 101),
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(10),
+                      alignment: Alignment.center,
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Icon(Icons.person), Text("Person")],
+                      ),
+                    ),
+                  ]),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Container(
                         width: 100,
                         height: 100,
-                        color: Color.fromARGB(255, 198, 78, 234),
+                        color: const Color.fromARGB(255, 180, 105, 103),
                         margin: EdgeInsets.all(10),
                         padding: EdgeInsets.all(10),
                         alignment: Alignment.center,
                         child: const Column(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          children: [Icon(Icons.book), Text("Book")],
+                          children: [Icon(Icons.phone), Text("Phone")],
                         ),
                       ),
                       Container(
                         width: 100,
                         height: 100,
-                        color: Color.fromARGB(255, 145, 83, 13),
+                        color: Color.fromARGB(181, 140, 108, 150),
                         margin: EdgeInsets.all(10),
                         padding: EdgeInsets.all(10),
                         alignment: Alignment.center,
                         child: const Column(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          children: [Icon(Icons.camera), Text("Camera")],
+                          children: [Icon(Icons.notes), Text("Notes")],
                         ),
                       ),
                       Container(
                         width: 100,
                         height: 100,
-                        color: Color.fromARGB(255, 134, 183, 101),
+                        color: Color.fromARGB(255, 130, 121, 133),
                         margin: EdgeInsets.all(10),
                         padding: EdgeInsets.all(10),
                         alignment: Alignment.center,
                         child: const Column(
                           mainAxisAlignment: MainAxisAlignment.center,
-                          children: [Icon(Icons.person), Text("Person")],
+                          children: [Icon(Icons.music_note), Text("Music")],
                         ),
                       ),
                     ],
